313545d443660cfd2455363d2a4ce4c5836192ad
SjxSubham <sjxsubham@gmail.com>
2024-11-05 00:43:20 +0530

    blank

--- frontend/Sjx_chat/src/pages/home/Home.jsx
+++ frontend/Sjx_chat/src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import MessageContainer from '../../components/messages/MessageContainer';
 import Sidebar from '../../components/sidebar/Sidebar';
-
+// make this project complete soon
 const Home = () => {
   return <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
       
