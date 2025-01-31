0c495f34f583601b900cb066c4192e4337596abe
SjxSubham <sjxsubham@gmail.com>
2025-02-01 00:53:04 +0530

    ok

--- frontend/Sjx_chat/src/zustand/useConversation.js
+++ frontend/Sjx_chat/src/zustand/useConversation.js
@@ -5,5 +5,5 @@ const useConversation = create((set) => ({
     setSelectedConversation: (selectedConversation) => set({ selectedConversation}),
     messages: [],
     setMessages: (messages) => set({ messages }),
-}))
+}));
 export default useConversation;
