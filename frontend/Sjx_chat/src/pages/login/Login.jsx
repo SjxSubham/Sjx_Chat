51b1806e744145e03f721879fd60f46da337919e
SjxSubham <sjxsubham@gmail.com>
2025-02-01 22:28:16 +0530

    remove whitespace support

--- frontend/Sjx_chat/src/pages/login/Login.jsx
+++ frontend/Sjx_chat/src/pages/login/Login.jsx
@@ -14,6 +14,12 @@ const Login = () => {
     await login(username, password);
   };
 
+  const handleUsernameKeyDown = (e) => {
+    if (e.key === ' ') {
+      e.preventDefault();
+    }
+  };
+
   return (
   <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
     <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -33,6 +39,7 @@ const Login = () => {
 							className='w-full input input-bordered h-10'
 							value={username}
 							onChange={(e) => setUsername(e.target.value)}
+              onKeyDown={handleUsernameKeyDown}
 						/>
 					</div>
 
