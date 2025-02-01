478e89f14d03aba291dad26ac5198f3550363286
SjxSubham <sjxsubham@gmail.com>
2025-02-02 02:46:10 +0530

    f

--- frontend/Sjx_chat/src/main.jsx
+++ frontend/Sjx_chat/src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <AuthContextProvider>
         <SocketContextProvider>
           <App />
-        <App />
+        
         </SocketContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
