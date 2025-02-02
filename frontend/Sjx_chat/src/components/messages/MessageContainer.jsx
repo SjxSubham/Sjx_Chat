a52ee98035794dda6cf6d7b08ec1c4c1ae91f97f
SjxSubham <sjxsubham@gmail.com>
2025-02-02 19:17:03 +0530

    ok

--- frontend/Sjx_chat/src/components/messages/MessageContainer.jsx
+++ frontend/Sjx_chat/src/components/messages/MessageContainer.jsx
@@ -20,7 +20,7 @@ const MessageContainer = () => {
          <>
             {/* Header */}
             <div className='bg-slate-500 px-4 py-2 mb-2 '>
-              <span className = 'label-text'>TO:</span>{" "}
+              <span className = 'label-text'>TO: </span>{" "}
               <span className='text-gray-800  font-bold'>{selectedConversation.fullname}</span>
             </div>
             <Messages />
