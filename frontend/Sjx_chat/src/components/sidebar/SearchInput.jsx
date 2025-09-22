bd8d89f6fe696a689e191dbefc52eaf53399fc46
SjxSubham <sjxsubham@gmail.com>
2025-09-23 00:43:09 +0530

    search input panel corner fixing

--- frontend/Sjx_chat/src/components/sidebar/SearchInput.jsx
+++ frontend/Sjx_chat/src/components/sidebar/SearchInput.jsx
@@ -28,7 +28,7 @@ const SearchInput = () => {
 			<input
 				type='text'
 				placeholder='Search…'
-				className='input input-bordered rounded-full'
+				className='input input-bordered rounded-2xl'
 				value={search}
 				onChange={(e) => setSearch(e.target.value)}
 			/>
