a6cac402a53bf9802fb596b72502be835addfd1c
SjxSubham <sjxsubham@gmail.com>
2025-10-26 19:00:38 +0530

    inxcreased the view point little bit

--- frontend/Sjx_chat/src/pages/home/Home.jsx
+++ frontend/Sjx_chat/src/pages/home/Home.jsx
@@ -1,18 +1,13 @@
-import MessageContainer from '../../components/messages/MessageContainer';
-import Sidebar from '../../components/sidebar/Sidebar';
+import MessageContainer from "../../components/messages/MessageContainer";
+import Sidebar from "../../components/sidebar/Sidebar";
 // make this project complete soon
 const Home = () => {
   return (
-  <div className='flex sm:h-[800px] md:h-[600px] rounded-lg overflow-hidden shadow-2xl bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-5'>
-      
-        <Sidebar />
-        <MessageContainer /> 
-   
-    
-    
-  </div>
+    <div className="flex h-screen sm:h-screen md:h-screen lg:h-screen rounded-lg overflow-hidden shadow-2xl bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-5">
+      <Sidebar />
+      <MessageContainer />
+    </div>
   );
-  
 };
 
-export default Home
+export default Home;
